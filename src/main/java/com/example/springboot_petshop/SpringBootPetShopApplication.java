e51ce4d1fec119fc8411b828ac41b08a461c2270
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-08-03 16:49:32 +0700

    Initial commit

---
+++ src/main/java/com/example/springboot_petshop/SpringBootPetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetShopApplication.class, args);
+	}
+
+}
